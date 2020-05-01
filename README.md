2104562a74888d36afdc3db8eb761d969ac2408d
Logan R Davis <logan@loganrdavis.com>
2020-04-30 21:26:10 -0500

    Initial, basic site created

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # should-i-buy-this-one
+My first project involving Django and web scrapers.
+Goal: This website will allow a user to input a product name and then get a recommended product from a site such as amazon.
---
+++ scraper/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Lookup(models.Model):
+    createdDate = models.DateTimeField(auto_now_add=True)
+    requestIp = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    resultUrl = models.CharField(max_length=2083)
+
+    def __str__(self):
+        return self.name
